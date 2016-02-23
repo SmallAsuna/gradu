97a85040d5f164b0079bb776a952a690b10d3150
SmallAsuna <assuna@foxmail.com>
2016-02-23 22:54:21 +0800

    2.0new
    新版的设计

---
+++ Application/Home/Controller/UserController.class.php
@@ -0,0 +1,16 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+class UserController extends Controller {
+    public function index(){
+      //  $this->show('NIHIHIJIJIJ');
+    	echo "你好";
+    
+    }
+
+    public function show(){
+       // $this->show('已经访问这个函数');
+    	echo "openssl_free_key";
+    
+    }
+}
---
+++ Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
@@ -0,0 +1,7 @@
+<?php if (!defined('THINK_PATH')) exit();?><html>
+
+<body>
+
+<h1>hello world <?php echo ($hello); ?> </h1>
+</body>
+</html>
