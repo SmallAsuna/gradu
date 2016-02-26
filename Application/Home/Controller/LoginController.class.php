7c3f2418f00365a6f08659f6fce97d4cc4a6bb8f
SmallAsuna <assuna@foxmail.com>
2016-02-26 10:04:55 +0800

    2.2.2
    更新了登录框的改进方案

--- Application/Home/Controller/LoginController.class.php
+++ Application/Home/Controller/LoginController.class.php
@@ -10,8 +10,23 @@ class LoginController extends Controller {
 
     public function doLogin(){
     	dump($_POST);
-    	echo $_POST["usern"];
-    }
+    	//echo $_POST["usern"];
+    	$User = M("User");
+    	$where['username']=$_POST["usern"];
+    	$where['password']=$_POST["passw"];
+    	$data = $User->field('username')->where($where)->find();
+    	if($data){
+    			dump($data);
+    			//$_SESSION['username']=$_POST["usern"];
+    			//echo $_SESSION["username"];
+    			unset($_SESSION['username']);
+    			echo $_SESSION["username"];
+    	}
+    	else{
+          echo "用户名或者密码错误";
+       }
+       //dump($data);
+       }
 
 
 
