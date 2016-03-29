4eb1a1a88b3f1929ca73d7d6be676d5c6e1760c5
SmallAsuna <assuna@foxmail.com>
2016-03-29 16:32:45 +0800

    Revert "5.59"
    This reverts commit 1d84fca48d03df160601b2a78567b711b6aed3f3.

--- Application/Home/Controller/MachineController.class.php
+++ Application/Home/Controller/MachineController.class.php
@@ -48,32 +48,12 @@ class MachineController extends Controller {
         $data['name']=$_POST["name"];
         $data['status']=$_POST["status"];
         $data['owner']=$_POST["owner"];
-        $data['price']=$_POST["price"];  
+        $data['price']=$_POST["price"];
+        
+        $Equipment->add($data);
+      
 
     }
-
-
-    public function upload(){
-    $upload = new \Think\Upload();// 实例化上传类
-    $upload->maxSize   =     3145728 ;// 设置附件上传大小
-    $upload->exts      =     array('pdf', 'gif', 'png', 'jpeg');// 设置附件上传类型
-    $upload->rootPath  =     './Uploads/'; // 设置附件上传根目录
-    $upload->savePath  =     ''; // 设置附件上传（子）目录
-    // 上传文件
-    $info   =   $upload->upload();
-    if(!$info) {// 上传错误提示错误信息
-    $this->error($upload->getError());
-    }
-
-    //else{// 上传成功 获取上传文件信息
-    //foreach($info as $file){
-    // echo $file['savepath'].$file['savename'];}
-    //}
-    }
-
-
-
-
 
     public function beuse($id){
 
